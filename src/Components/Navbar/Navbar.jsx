d6176d1fe9909a949c0c74d337e24365d38275fb
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2022-12-19 11:24:48 +0100

    initializing react app

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div className="nav__logo">
+        <a>MyTestApp</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
